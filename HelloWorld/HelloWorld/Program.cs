43784cc1263662e1461f443adce79d1e8295f20e
Kusumapriya1525 <kusumapriyagurubilli@gmail.com>
2022-10-17 20:35:22 +0530

    after adding a new line in program

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello new repository");
             Console.WriteLine("Hello Github");
         }
     }
